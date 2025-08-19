81260280ea0b2599685e9b10f6f7fe3b82139ed0
shir kocurek <shir617@gmail.com>
2025-08-18 21:28:36 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -203,8 +203,8 @@ const Footer: React.FC = () => {
           </div>
         </div>
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-xs leading-5 text-gray-400">
-            &copy; 2025 Shir Kocurek, Inc. All rights reserved.
+          <p className="text-xs leading-5 pb-5 text-gray-400">
+            &copy; 2025 SK, Inc. All rights reserved.
           </p>
         </div>
       </div>
